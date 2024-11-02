562701c43582f07ebb94bd5069063f9a9df72ec4
Valentino Lorenzati <valentinolorenzati@gmail.com>
2024-11-01 22:56:00 -0300

    Apartado .10
    exportacion de archivos con las busquedas

--- proyecto final/Aplicacion/gestion_usuarios.py
+++ proyecto final/Aplicacion/gestion_usuarios.py
@@ -1,9 +1,11 @@
+import os
+from datetime import datetime
 # gestion_usuarios.py
 import pickle
 #from utilidades import cargar_usuarios, guardar_usuarios, ordenar_usuarios, ver_ispc
 from modelos import Usuario
-
 #Funciones Gestion Usuarios:
+
 
 def cargar_usuarios():
     try:
@@ -17,6 +19,8 @@ def guardar_usuarios(usuarios):
     usuarios_ordenados = dict(sorted(usuarios.items(), key=lambda item: item[1].dni))
     with open('usuarios.ispc', 'wb') as f:
         pickle.dump(usuarios_ordenados, f)
+
+
 
 
 def ordenar_burbuja(usuarios):
@@ -196,14 +200,24 @@ def eliminar_usuario():
     print(f"Usuario {username} eliminado con éxito.")
 
 def buscar_usuario():
-    usuarios = cargar_usuarios()
-    username = input("Ingrese el username a buscar: ")
-
-    if username in usuarios:
-        print(usuarios[username])
+    opcion = input("¿Buscar por (1) Username, (2) DNI, (3) Email? ")
+    
+    if opcion == '1':
+        username = input("Ingrese el username a buscar: ")
+        encontrado, intentos = buscar_usuario_por_username(username)
+        if not encontrado:
+            print("No se encontró el usuario con dicho username.")
+    
+    elif opcion == '2':
+        dni = input("Ingrese el DNI a buscar: ")
+        buscar_usuario_por_dni(dni)
+    
+    elif opcion == '3':
+        email = input("Ingrese el email a buscar: ")
+        buscar_usuario_por_email(email)
+    
     else:
-        print("Usuario no encontrado.")
-
+        print("Opción no válida.")
 
 
 #----------------------------------------------------------------------#
@@ -265,4 +279,154 @@ def mostrar_usuarios():
         elif opcion == "2":
             ordenar_usuarios_por_username()
             ver_ispc_ordenados_usuario()
+
+#----------------------------------------
+
+def cargar_usuarios_ordenados_por_username():
+    # Supongamos que estás cargando usuarios desde un archivo usando pickle
+    import pickle
+    
+    # Cargar usuarios desde el archivo
+    with open('usuariosordenadosporusername.ispc', 'rb') as file:
+        usuarios_dict = pickle.load(file)
+    
+    # Convertir el diccionario a una lista
+    usuarios_ordenados = list(usuarios_dict.values())
+    
+    # Ordenar la lista por username (opcional, si ya está ordenada)
+    usuarios_ordenados.sort(key=lambda usuario: usuario.username)
+
+    return usuarios_ordenados
+
+carpeta_busquedas = os.path.dirname(os.path.abspath(__file__)) + "/busquedasYordenamientos"
+if not os.path.exists(carpeta_busquedas):
+    os.makedirs(carpeta_busquedas)
+
+def buscar_usuario_por_username(username):
+    usuarios = cargar_usuarios()  # Cargar usuarios de tu archivo
+    intentos = 0
+    log_file_path = os.path.join(carpeta_busquedas, f'buscandoUsuarioPorUsername-{datetime.now().strftime("%Y%m%d-%H%M%S")}.txt')
+
+    # Abrir el archivo de log antes de comenzar la búsqueda
+    with open(log_file_path, 'a') as log:
+        log.write(f"Búsqueda por Username: buscando el username '{username}'.\n")
+
+        # Verificar si existe el archivo de usuarios ordenados
+        if os.path.exists("usuariosOrdenadosPorUsername.ispc"):
+            usuarios_ordenados = cargar_usuarios_ordenados_por_username()
+            intentos, encontrado = busqueda_binaria_username(usuarios_ordenados, username, log_file_path, intentos)
+            if encontrado:
+                return True, intentos
+            else:
+                log.write(f"Usuario no encontrado usando búsqueda binaria.\n")
+                print(f"Usuario no encontrado usando búsqueda binaria.")
+
+        # Si no se encontró, intentar búsqueda secuencial en el archivo original
+        log.write(f"Buscando el username '{username}' en el archivo original con búsqueda secuencial...\n")
+        for usuario in usuarios.values():
+            intentos += 1
+            log.write(f"Intento {intentos}: Comparando '{username}' con '{usuario.username}'.\n")
+            print(f"Intento {intentos}: '{username}' es distinto a '{usuario.username}'")
+            if usuario.username == username:
+                print(f"Usuario encontrado: {usuario}")
+                log.write(f"Username encontrado en {intentos} intentos.\n")
+                return True, intentos
+
+        print("Usuario no encontrado.")
+        log.write(f"Se realizaron {intentos} intentos y no se encontró el username buscado.\n")
+    return False, intentos
+
+def buscar_usuario_por_dni(dni):
+    usuarios = cargar_usuarios()
+    intentos = 0
+    usuarios_ordenados = sorted(usuarios.values(), key=lambda x: x.dni)
+    log_file = os.path.join(carpeta_busquedas, f'buscandoUsuarioPorDNI-{datetime.now().strftime("%Y%m%d-%H%M%S")}.txt')
+
+    with open(log_file, 'w') as log:
+        log.write(f"Búsqueda Binaria por DNI: buscando el DNI '{dni}' en el archivo usuarios.ispc que contiene {len(usuarios_ordenados)} usuarios.\n")
+
+        if not usuarios_ordenados:
+            print("No hay usuarios registrados.")
+            return
+
+        if dni < usuarios_ordenados[0].dni:
+            print(f"No se encuentra registrado el usuario con el DNI '{dni}' debido a que el DNI a buscar es más chico que el más chico de los registrados.")
+            return
+        if dni > usuarios_ordenados[-1].dni:
+            print(f"No se encuentra registrado el usuario con el DNI '{dni}' debido a que el DNI a buscar es más grande que el más grande de los registrados.")
+            return
+
+        inicio, fin = 0, len(usuarios_ordenados) - 1
+        while inicio <= fin:
+            mid = (inicio + fin) // 2
+            valor_campo = usuarios_ordenados[mid].dni
+            intentos += 1
+            log.write(f"Intento {intentos}: DNI del usuario de la posición {mid} es {valor_campo}.\n")
+
+            if valor_campo == dni:
+                print(f"Usuario encontrado: {usuarios_ordenados[mid]}")
+                log.write(f"DNI encontrado en {intentos} intentos.\n")
+                return True, intentos
+            elif valor_campo < dni:
+                log.write(f"Buscar en la subsecuencia de la derecha (posición {mid + 1} a {fin}).\n")
+                inicio = mid + 1
+            else:
+                log.write(f"Buscar en la subsecuencia de la izquierda (posición {inicio} a {mid - 1}).\n")
+                fin = mid - 1
+
+        log.write(f"Se realizaron {intentos} intentos y no se encontró el DNI buscado.\n")
+        print("Usuario no encontrado.")
+        return False, intentos
+
+
+def buscar_usuario_por_email(email):
+    usuarios = cargar_usuarios()
+    intentos = 0
+    print(f"Buscando el email '{email}'...")
+
+    for usuario in usuarios.values():
+        intentos += 1
+        print(f"Intento {intentos}: Comparando con '{usuario.email}'")
+        if usuario.email == email:
+            print(f"Usuario encontrado: {usuario}")
+            return True, intentos
+    
+    print("Usuario no encontrado.")
+    return False, intentos
+
+
+
+#################
+
+def busqueda_binaria_username(usuarios_ordenados, username, log_file, intentos):
+    inicio, fin = 0, len(usuarios_ordenados) - 1
+    with open(log_file, 'a') as log:
+        log.write(f"Búsqueda Binaria por Username: buscando el username '{username}'.\n")
+        
+        while inicio <= fin:
+            mid = (inicio + fin) // 2
+            valor_campo = usuarios_ordenados[mid].username
+            intentos += 1
+            
+            # Escribir en el log
+            log.write(f"Intento {intentos}: username del usuario de la posición {mid} es '{valor_campo}'.\n")
+            
+            # Imprimir en la consola
+            print(f"Intento {intentos}: username del usuario de la posición {mid} es '{valor_campo}'.")
+
+            if valor_campo == username:
+                print(f"Usuario '{username}' encontrado en el intento {intentos}.")
+                return intentos, True
+            elif valor_campo < username:
+                log.write(f"Buscar en la subsecuencia de la derecha (posición {mid + 1} a {fin}).\n")
+                print(f"Buscar en la subsecuencia de la derecha (posición {mid + 1} a {fin}).")
+                inicio = mid + 1
+            else:
+                log.write(f"Buscar en la subsecuencia de la izquierda (posición {inicio} a {mid - 1}).\n")
+                print(f"Buscar en la subsecuencia de la izquierda (posición {inicio} a {mid - 1}).")
+                fin = mid - 1
+
+    print(f"Usuario '{username}' no encontrado después de {intentos} intentos.")
+    return intentos, False
+
 
